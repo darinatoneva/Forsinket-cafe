f691fde16420e192f02646868b3b42cafb113a62
your actual name <your email address used to login at githubtonevadarina@gmail.com>
2020-11-28 18:04:19 +0100

    Mobile first for the index page

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Galleri1
+// var quadimages = document.querySelectorAll("#quad figure");
+// for (i = 0; i < quadimages.length; i++) {
+//     quadimages[i].addEventListener('click', function () { this.classList.toggle("expanded"); quad.classList.toggle("full") });
+// }
