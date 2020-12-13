d9d4ab20ba674626230faadc6bb5166c2e912564
your actual name <your email address used to login at githubtonevadarina@gmail.com>
2020-12-13 12:40:47 +0100

    the main styling is finished

--- script.js
+++ script.js
@@ -4,3 +4,4 @@
 // for (i = 0; i < quadimages.length; i++) {
 //     quadimages[i].addEventListener('click', function () { this.classList.toggle("expanded"); quad.classList.toggle("full") });
 // }
+
